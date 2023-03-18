dbbf2b31ea23fcecb504f25c0b3267c3cbe070e6
Shehab Ihab <shehabihab@hotmail.com>
2023-03-17 17:30:08 -0700

    100-print_comb3.c and 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++) {
+		for (j = i + 1; j <= 9; j++) {
+			for (k = j + 1; k <= 9; k++) {
+				if (i != j && j != k && i != k) {
+					printf("%d%d%d", i, j, k);
+					if (i != 7)
+						printf(", ");
+					else
+						printf("\n");
+				}
+			}
+		}
+	}
+	return (0);
+}
