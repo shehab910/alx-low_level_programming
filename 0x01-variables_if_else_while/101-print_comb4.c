a3aadce9cd7577fdb3a1b1cace35347c8c97da50
Shehab Ihab <shehabihab@hotmail.com>
2023-03-17 17:36:26 -0700

    100 and 101 fixed

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,10 +9,14 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = 0; i <= 9; i++) {
-		for (j = i + 1; j <= 9; j++) {
-			for (k = j + 1; k <= 9; k++) {
-				if (i != j && j != k && i != k) {
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
+				if (i != j && j != k && i != k)
+				{
 					printf("%d%d%d", i, j, k);
 					if (i != 7)
 						printf(", ");
