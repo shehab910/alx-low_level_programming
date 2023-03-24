c8d15fc6b42d72e7bd227e323951aa00194743ae
Shehab Ihab <shehabihab@hotmail.com>
2023-03-23 19:35:40 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - check if upper.
+ *
+ * Return: return 1 if is upper.
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
