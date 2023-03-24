9b282463a047fe00dd4cef78f00b3e3a6411855e
shehab910 <shehabihab@hotmail.com>
2023-03-24 14:23:58 +0200

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul -  prints the numbers, from 0 to 9, followed by a new line.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+}
--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -6,7 +6,7 @@
  * Return: on success 1.
  * on error, -1 is returned, and errno is set appropriately.
 */
-int_ putchar (char c)
+int _putchar (char c)
 {
 	return (write (1, &c, 1));
 }
