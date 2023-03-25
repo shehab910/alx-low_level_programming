242aebe0501f0e4571edb42dd7ca02c1f17ca026
shehab910 <shehabihab@hotmail.com>
2023-03-25 03:22:55 +0200

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_number - print num
+ *
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	if (n / 10 > 0)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
+}
