235a3a0d8c2972e94b79a91a587504e4a22e873e
shehab910 <shehabihab@hotmail.com>
2023-03-28 02:57:50 +0200

    main.h fixed 3,4

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * _puts - prints a string, followed by a new line, to stdout.
  * @str: string to print
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,5 +5,7 @@
 
 int _putchar(char c);
 void reset_to_98(int *n);
+void _puts(char *str);
+void print_rev(char *s);
 
 #endif /* MAIN_H */
