995d1cb1704765436344f61cf0b689c3eba1c402
shehab910 <shehabihab@hotmail.com>
2023-04-01 04:11:55 +0200

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcat - check the code
+ * @dest: dest
+ * @src: src
+ * Return: string
+ */
+char *_strcat(char *dest, char *src)
+{
+		char *p = dest;
+		while (*dest != '\0')
+				dest++;
+		while (*src != '\0')
+				*(dest++) = *(src++);
+		*dest = '\0';
+		return p;
+}
