dfe8bae204a1a91f2cdfe751c6fbf44d1db5575a
shehab910 <shehabihab@hotmail.com>
2023-04-18 04:57:35 +0200

    updated dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,11 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 typedef struct dog dog_t;
 
 #endif /* DOG_H */
