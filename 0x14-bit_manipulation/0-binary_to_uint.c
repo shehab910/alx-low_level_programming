f29c3c4ffef4ac2b228fc6ae5b9f059686f8d2a2
shehab910 <shehabihab@hotmail.com>
2023-05-05 01:28:16 +0300

    0x14-0 fixed betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,44 +1,42 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
  * twoPower - compute the the power of a number for base 2
- * @base: the base
  * @pow: the power
  * Return: result
  */
-unsigned int twoPower(int power)
+unsigned int twoPower(int pow)
 {
 	unsigned int value = 1;
 
-	while (power--)
+	while (pow--)
 		value *= 2;
 	return (value);
 }
 /**
  * binary_to_uint - converts a binary number to an unsigned int.
- *
- * Return: 
+ * @b: string containg binary number
+ * Return: the converted number, or 0 if
+ * there is one or more chars in the string b that is not 0 or 1
+ * b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
 	int len = 0, i;
 	unsigned int res = 0;
-	const char* tmp;
-	
-	if(!b)
-		return 0;
+	const char *tmp;
+
+	if (!b)
+		return (0);
 	tmp = b;
 	while (*(tmp++) != '\0')
 		len++;
-	for (i = len-1; i >= 0; i--)
+	for (i = len - 1; i >= 0; i--)
 	{
 		if (b[i] == '0')
 			continue;
 		else if (b[i] == '1')
-			res += twoPower(len-1-i);
+			res += twoPower(len - 1 - i);
 		else
-			return 0;
+			return (0);
 	}
 	return (res);
 }
