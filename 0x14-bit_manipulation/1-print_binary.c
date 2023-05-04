03d18b23284b7b10857a2a1ea6ca0d4680cb2b95
shehab910 <shehabihab@hotmail.com>
2023-05-05 02:23:30 +0300

    0x14-1 format fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,19 +11,24 @@
  */
 void print_binary(unsigned long int n)
 {
-	 unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
-    int flag = 0;
+	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	int flag = 0;
 
-    while (mask) {
-        if (n & mask) {
-            putchar('1');
-            flag = 1;
-        } else if (flag) {
-            putchar('0');
-        }
-        mask >>= 1;
-    }
-    if (!flag) {
-        putchar('0');
-    }
+	while (mask)
+	{
+		if (n & mask)
+		{
+			putchar('1');
+			flag = 1;
+		}
+		else if (flag)
+		{
+			putchar('0');
+		}
+		mask >>= 1;
+	}
+	if (!flag)
+	{
+		putchar('0');
+	}
 }
