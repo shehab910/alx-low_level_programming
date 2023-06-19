84c619f8d46edbd23db01675252bd42c318ad4d1
Shehab Ihab <shehabihab@hotmail.com>
2023-06-19 14:20:48 +0300

    0x18- 100-operations.so

---
+++ 0x18-dynamic_libraries/2-operations.c
@@ -0,0 +1,54 @@
+/**
+ * add - add two numbers
+ * @a: first number
+ * @b: second number
+ * Return: sum of two numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two numbers
+ * @a: first number
+ * @b: second number
+ * Return: difference of two numbers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply two numbers
+ * @a: first number
+ * @b: second number
+ * Return: product of two numbers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divide two numbers
+ * @a: first number
+ * @b: second number
+ * Return: quotient of two numbers
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulo two numbers
+ * @a: first number
+ * @b: second number
+ * Return: remainder of two numbers
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
