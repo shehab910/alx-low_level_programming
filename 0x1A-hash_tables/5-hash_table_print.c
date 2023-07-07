1b8c5f4c6b9585319ff9ad86282452fce8b8a3e1
Shehab Ihab <shehabihab@hotmail.com>
2023-07-07 21:51:46 +0300

    0x1A-5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+	char *sep;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	sep = "";
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			printf("%s'%s': '%s'", sep, node->key, node->value);
+			sep = ", ";
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
